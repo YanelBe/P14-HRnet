1e8a1035265e7d022f70727c60a41e1261a11cd2
YanelBe <yanel.bensa@gmail.com>
2024-03-08 12:17:27 +0100

    CSS Correction

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,14 +8,16 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
     return (
         <div className="header-container">
-            <h2>
-                <NavLink to="/" className="logo-title">
-                    HRnet
-                </NavLink>
-            </h2>
-            <div className="nav-right">
-                    <NavLink to="/" className="header-link">Add New Employee</NavLink>
-                    <NavLink to="/employees" className="header-link">View Current Employees</NavLink>
+            <div className="header-wrap">
+                <h2>
+                    <NavLink to="/" className="logo-title">
+                        HRnet
+                    </NavLink>
+                </h2>
+                <div className="nav-right">
+                        <NavLink to="/" className="header-link">Add New Employee</NavLink>
+                        <NavLink to="/employees" className="header-link">View Current Employees</NavLink>
+                </div>
             </div>
         </div>
     );
